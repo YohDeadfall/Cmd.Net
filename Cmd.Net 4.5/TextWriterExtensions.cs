2b238c3b5b726c1ae3bcde3407223905f7f11030
Yoh Deadfall <yoh.deadfall@hotmail.com>
2016-05-12 12:22:24 +0300

    Documentation.

--- Cmd.Net 4.5/TextWriterExtensions.cs
+++ Cmd.Net 4.5/TextWriterExtensions.cs
@@ -4,18 +4,23 @@ using System.IO;
 namespace Cmd.Net
 {
     /// <summary>
-    /// 
+    /// Provides utility methods that perform common tasks.
     /// </summary>
     public static class TextWriterExtensions
     {
         #region Public Methods
 
         /// <summary>
-        /// 
+        /// Writes a string to the text string or stream with indentation of every line of the string.
         /// </summary>
-        /// <param name="output"></param>
-        /// <param name="value"></param>
-        /// <param name="indent"></param>
+        /// <param name="output">A <see cref="T:System.IO.TextWriter" /> that represents an output stream.</param>
+        /// <param name="value">The string to write.</param>
+        /// <param name="indent">The space count to indent.</param>
+        /// <exception cref="T:System.ArgumentNullException"><paramref name="output" /> is null.</exception>
+        /// <exception cref="T:System.ArgumentOutOfRangeException"><paramref name="indent" /> is less than zero.</exception>
+        /// <remarks>
+        /// If <paramref name="value" /> is null, nothing is written to the text stream.
+        /// </remarks>
         public static void WriteIndented(this TextWriter output, string value, int indent)
         {
             if (output == null)
