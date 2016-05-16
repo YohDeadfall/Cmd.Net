e37e0a154ddc8d4637ef2b935f38f12002d6340a
Yoh Deadfall <yoh.deadfall@hotmail.com>
2016-05-16 11:49:32 +0300

    RemarksAttribute is inheritable.

--- Cmd.Net 4.5/RemarksAttribute.cs
+++ Cmd.Net 4.5/RemarksAttribute.cs
@@ -6,11 +6,11 @@ namespace Cmd.Net
     /// Specifies remarks for a command.
     /// </summary>
     [AttributeUsage(AttributeTargets.Method)]
-    public sealed class RemarksAttribute : Attribute
+    public class RemarksAttribute : Attribute
     {
         #region Fields
 
-        private readonly string _remarks;
+        private string _remarks;
 
         #endregion
 
@@ -40,9 +40,26 @@ namespace Cmd.Net
         /// Gets the remarks stored in this attribute.
         /// </summary>
         /// <value>The remarks stored in this attribute.</value>
-        public string Remarks
+        public virtual string Remarks
         {
             get { return _remarks; }
+        }
+
+        /// <summary>
+        /// Gets or sets the string stored as the remarks.
+        /// </summary>
+        /// <value>The string stored as the remarks.</value>
+        /// <remarks>
+        /// <para>The default implementation of the <see cref="M:Cmd.Net.RemarksAttribute.Remarks" /> property simply returns this value.</para>
+        /// <para>This extra property exists so that you can derive from <see cref="T:Cmd.Net.RemarksAttribute" /> and provide a localized version.
+        /// The derived localizable <see cref="T:Cmd.Net.RemarksAttribute" /> will maintain a private Boolean field to indicate if it has been localized.
+        /// On the first access to the <see cref="M:Cmd.Net.RemarksAttribute.Remarks" /> property, it will look up the localized string and store it back
+        /// in the RemarksValue property.</para>
+        /// </remarks>
+        protected string RemarksValue
+        {
+            get { return _remarks; }
+            set { _remarks = value; }
         }
 
         #endregion
